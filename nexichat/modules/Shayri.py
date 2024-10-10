cbf7f1f188d350ef461cecad08f79a666d61d408
Danishzain05 <163726214+Danishzain05@users.noreply.github.com>
2024-10-10 18:42:27 +0530

    Update Shayri.py

--- nexichat/modules/Shayri.py
+++ nexichat/modules/Shayri.py
@@ -75,10 +75,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"
+                        "✨SUPPORT✨", url=f"https://t.me/LinkZone_05"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS"
+                        "✨OWNER✨", url=f"https://t.me/ZainXd_05"
                     ),
                 ]
             ]
